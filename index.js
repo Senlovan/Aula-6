f7c4e18e293bf3baccdf000b4d8609d9fd473c54
Senlovan <maiconmattossenlovan@gmail.com>
2023-05-03 21:21:07 -0300

    Subindo Aula 6

---
+++ index.js
@@ -0,0 +1,49 @@
+/*const altura = 2
+const largura = 3
+const area = altura * largura
+
+
+function calcularArea(numAlt,numLar){
+    //ação da minha função
+const altura = numAlt;
+const largura = numLar;
+const area = altura * largura
+return area;
+}
+
+
+console.log(calcularArea(10,22));
+console.log(calcularArea(15,90));
+console.log(calcularArea(12,45));
+console.log(calcularArea(10,700));*/
+
+/*function imprimirOlaMundo (){
+    console.log("olá Mundo")
+}
+
+imprimirOlaMundo()*/
+
+/*function mostraNome(nome){
+    console.log("Olá",$[nome])
+return nome
+}
+mostraNome("Maicon")
+mostraNome("José")
+mostraNome("Felipe")*/
+
+/*function colocarNomeMaiusculo(nome){
+    const nomeMaiusculo = nome.toUpperCase()
+    return nomeMaiusculo
+}
+console.log(colocarNomeMaiusculo("maria antonieta"));*/
+
+/*function somar(numero1,numero2){
+    const soma = numero1 + numero2
+    return soma
+}
+const retornoSoma = somar(10,20)
+console.log(retornoSoma);*/
+
+function recebaArray (num1,num2){
+    const array = 
+}
